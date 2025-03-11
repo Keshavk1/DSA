066923ca0963a6b030b0db387808a594bf7d9f79
KESHAV MEENA <mrkeshavms@gmail.com>
2025-03-11 12:42:18 +0530

    Create Day05.cpp

---
+++ Day05.cpp
@@ -0,0 +1,23 @@
+// longest palindrome 
+
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        map <int,int> hash;
+        for(int i = 0 ; i<s.length() ; i++){
+            hash[s[i]-'0']++;
+        }
+        int cnt = 0;
+        for(auto it : hash){
+            if(it.second >= 2){
+                cnt += it.second/2;
+            }
+        }
+        if((cnt*2) == s.length()){
+        return (cnt*2);
+        }
+        return (cnt*2)+1;
+    }
+};
+
+// DISTRIBUTE Money
