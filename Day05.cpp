af242b89246eb2f2676cbca7ea013758318e4b6d
KESHAV MEENA <mrkeshavms@gmail.com>
2025-03-11 17:59:34 +0530

    Day05.cpp

--- Day05.cpp
+++ Day05.cpp
@@ -21,3 +21,25 @@ public:
 };
 
 // DISTRIBUTE MONEY :-
+class Solution {
+public:
+    int distMoney(int money, int children) {
+        if (money < children){
+            return -1;
+        }
+        money -= children;
+        int count = 0;
+        while (money >= 7 && children > 0) {
+            money -= 7;
+            count++;
+            children--;
+        }
+        if (children == 1 && money == 3) {
+            count--;
+        }
+        if (children == 0 && money > 0) {
+            return count - 1;
+        }
+        return count;
+    }
+};
