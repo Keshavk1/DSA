9d6870fb9cb205be355ebddb2a1e3ddb5c93a313
KESHAV MEENA <mrkeshavms@gmail.com>
2025-03-23 12:06:59 +0530

    Day16.cpp

--- Day16.cpp
+++ Day16.cpp
@@ -1,1 +1,17 @@
-
+// First unique charcter
+class Solution {
+public:
+    int firstUniqChar(string s) {
+     vector<int> f(26,0);
+     for(auto ch : s){
+        f[ch-'a']++;
+     }
+     for(int i = 0; i<s.length(); i++){
+        if(f[s[i]-'a'] == 1){
+            return i;
+        }
+     }
+     return -1;
+    }
+};
+// hashing
