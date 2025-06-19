1ba3e00635ee87573ee2021dba7d8b23e65cdd2c
BABEL-HUB <mrkeshavms@gmail.com>
2025-06-19 19:40:32 +0530

    Adding Two Sum and Best time to buy and Sell stock

--- 01_Arrays/01_TwoSum.cpp
+++ 01_Arrays/01_TwoSum.cpp
@@ -1,14 +1,55 @@
 /*
-Problem: [Problem Name]
-Link: [LeetCode/HackerRank/GFG Link]
-Date: [Date]
-Approach: [Brief explanation of your approach]
+Problem: Two Sum
+Link: https://leetcode.com/problems/two-sum/description/
+Approach: 
+ 1. One brute force approach is to consider every pair of elements and check if their sum equals the target. This can be done using nested loops, where the   outer loop iterates from the first element to the second-to-last element, and the inner loop iterates from the next element to the last element. However, this approach has a time complexity of O(n^2).
+ 2. A more efficient approach is to use a hash table (unordered_map in C++). We can iterate through the array once, and for each element, check if the target minus the current element exists in the hash table. If it does, we have found a valid pair of numbers. If not, we add the current element to the hash table.
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    // Your code here
-    return 0;
-}
+
+// Brute force
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return {i, j};
+                }
+            }
+        }
+        return {}; // No solution found
+    }
+};
+
+
+
+// optimal Approach
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> mpp;
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            int complement = target - nums[i];
+            if (mpp.count(complement))
+            {
+                return {mpp[complement], i}; // return indexes
+            }
+            mpp[nums[i]] = i; // storing indexes
+        }
+        return {}; // No solution
+    }
+};
--- 01_Arrays/02_BestTimeToBuyAndSellStock.cpp
+++ 01_Arrays/02_BestTimeToBuyAndSellStock.cpp
@@ -1,14 +1,25 @@
 /*
-Problem: [Problem Name]
-Link: [LeetCode/HackerRank/GFG Link]
-Date: [Date]
-Approach: [Brief explanation of your approach]
+Problem: Best-time-to-buy-and-sell-stock
+Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+Approach:
+    The problem aims to find the maximum profit that can be obtained by buying and selling a stock. The given solution seems to follow a simple approach of iterating through the prices, keeping track of the minimum buying price, and updating the profit whenever a higher selling price is encountered.
 */
 
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    // Your code here
-    return 0;
-}
+class Solution
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
+        int minprice = INT_MAX;
+        int maxprofit = 0;
+        for (int i = 0; i < prices.size(); i++)
+        {
+            minprice = min(minprice, prices[i]);
+            maxprofit = max(maxprofit, prices[i] - minprice);
+        }
+        return maxprofit;
+    }
+};
